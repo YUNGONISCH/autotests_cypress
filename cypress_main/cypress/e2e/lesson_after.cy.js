cdc619cfb6f1dc3c5906f165976fc02293bc143d
Владислав Онищенко <vladzcbm@gmail.com>
2025-01-20 01:10:48 +0300

    Add files via upload

---
+++ cypress_main/cypress/e2e/lesson_after.cy.js
@@ -0,0 +1,44 @@
+describe('Проверка авторизации', function () {
+
+   beforeEach('Начало теста', function () {
+         cy.visit('/');
+         cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+           });
+
+   afterEach('Конец теста', function () {
+         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+        });
+
+   it('Верный пароль и верный логин', function () {
+        cy.get('#mail').type('USER_LOGIN');
+        cy.get('#pass').type('USER_PASSWORD');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Авторизация прошла успешно');
+    })
+
+      it('Верный логин и неверный пароль', function () {
+        cy.get('#mail').type('USER_LOGIN');
+        cy.get('#pass').type('xxxxxxxxxxxxx');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+    })
+      it('Валидация на наличие @', function () {
+        cy.get('#mail').type('USER_LOGIN');
+        cy.get('#pass').type('xxxxx.xx');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Нужно исправить проблему валидации');
+    })
+
+       it('Восстановление пароля', function () {
+        cy.get('#forgotEmailButton').click();
+        cy.get('#mailForgot').type('USER_LOGIN');
+        cy.get('#restoreEmailButton').click();
+        cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
+    })
+})
+
+
+// npx cypress run --spec cypress/e2e/poke.cy.js --browser chrome
---
+++ cypress_main/cypress/e2e/lesson_baseUrl.cy.js
@@ -0,0 +1,49 @@
+describe('Проверка авторизации', function () {
+
+   it('Верный пароль и верный логин', function () {
+        cy.visit('/');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#mail').type('USER_LOGIN');
+        cy.get('#pass').type('USER_PASSWORD');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Авторизация прошла успешно');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+
+    })
+
+      it('Верный логин и неверный пароль', function () {
+        cy.visit('/');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#mail').type('USER_LOGIN');
+        cy.get('#pass').type('xxxxxxxxxxxxx');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+      it('Валидация на наличие @', function () {
+        cy.visit('/');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#mail').type('USER_LOGIN');
+        cy.get('#pass').type('iLoveqastudio');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Нужно исправить проблему валидации');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+       it('Восстановление пароля', function () {
+        cy.visit('/');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#forgotEmailButton').click();
+        cy.get('#mailForgot').type('USER_LOGIN');
+        cy.get('#restoreEmailButton').click();
+        cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+})
+
+
+// запуск через теринал: npx cypress run --spec cypress/e2e/poke.cy.js --browser chrome
